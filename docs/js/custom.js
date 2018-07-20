494add6a1d92f168b2d8336e8e1a0dee9cd444a7
wangrui04 <wangrui04@shengpay.com>
2018-07-20 14:45:47 +0800

    commit

---
+++ docs/js/custom.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4170e99929bbed3444ed584e17d946dc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
